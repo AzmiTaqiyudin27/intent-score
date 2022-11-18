040595941969433158c50a11a11ec4977aa7d230
AzmiTaqiyudin27 <azmi.taqiyudin@mhs.unsoed.ac.id>
2022-11-18 14:13:19 +0700

    Azmi Taqiyudin    H1D020046 SHIFT G

--- app/src/main/java/id/putraprima/skorbola/MainActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         //5. Next Button Pindah Ke MatchActivity
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
